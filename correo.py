8d13e043644e20cfa51ab4ed8e69646a7ab2ecb4
Davies2025 <es234100097@uma.edu.sv>
2025-03-02 15:31:10 -0600

    Eliminando calculadora.py y modificando correo.py

--- correo.py
+++ correo.py
@@ -1,3 +1,26 @@
 
+print ("BIENVENIDO A MI CALCULADORA BÁSICA")
 
+print ("Escribe el número de la operación que deseas usar:")
+
+operacion = int(input("1.SUMA 2.RESTA 3.Multiplicación 4.División "))
+
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+if operacion == 1: 
+    resultado1 = numero1 + numero2 
+    print (resultado1)
+
+elif operacion == 2:
+    resultado2 = numero1 - numero2 
+    print (resultado2)
+
+elif operacion == 3:
+    resultado3 = numero1 * numero2 
+    print (resultado3)
+
+elif operacion == 4:
+    resultado4 = numero1 / numero2 
+    print (resultado4)
 
